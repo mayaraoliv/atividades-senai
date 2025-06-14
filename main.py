05d414a172cf169002da808c78bce2f506c14e54
Mayara Oliveira <mayara.inacio197@gmail.com>
2025-06-14 14:47:22 -0300

    Commit final.

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-...
+from produtos_classes import Produto, ProdutoImportado, ProdutoNacional
+
+#pedido do exercício 1
+p = Produto("Teclado",100.0,20)
+p.exibir_detalhes()
+
+#pedido do exercício 2
+print()
+c = ProdutoImportado("Celular",200.0,5,0.15)
+c.exibir_detalhes()
+
+#pedido do exercício 3
+print()
+c.preco_final()
+n = ProdutoNacional("Cadeira", 150.0, 10)
+n.preco_final()
+
+#teste do exercício 4
+print()
+c.emitir_nota()
+n.emitir_nota()
+
+#teste do exercício 5
+print()
+a=Produto("Chinelo",150,5)
+a.repor(5)
+a.vender(5)
--- produtos_classes.py
+++ produtos_classes.py
@@ -47,29 +47,3 @@ class ProdutoImportado(Produto):
     
     def emitir_nota(self):
         print(f"Nota de importação para {self.nome} com taxa aplicada.")
-
-#pedido do exercício 1
-p = Produto("Teclado",100.0,20)
-p.exibir_detalhes()
-
-#pedido do exercício 2
-print()
-c = ProdutoImportado("Celular",200.0,5,0.15)
-c.exibir_detalhes()
-
-#pedido do exercício 3
-print()
-c.preco_final()
-n = ProdutoNacional("Cadeira", 150.0, 10)
-n.preco_final()
-
-#teste do exercício 4
-print()
-c.emitir_nota()
-n.emitir_nota()
-
-#teste do exercício 5
-print()
-a=Produto("Chinelo",150,5)
-a.repor(5)
-a.vender(5)
