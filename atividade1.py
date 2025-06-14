8077d4b7cd9a8de1fc66afb9f05573d4676b56c1
Mayara Oliveira <mayara.inacio197@gmail.com>
2025-06-14 11:57:35 -0300

    Exercício 2 concluído =)

--- atividade1.py
+++ atividade1.py
@@ -5,6 +5,21 @@ class Produto:
         self.estoque=estoque
     def exibir_detalhes(self):
         print(f"Produto:{self.nome} | Preço: R${self.preco} | Estoque:{self.estoque} unidades")
-        pass
+
 p=Produto("Teclado",100.0,20)
 p.exibir_detalhes()
+
+class ProdutoNacional(Produto):
+    def __init__(self,nome,preco,estoque):
+        super().__init__(nome,preco,estoque)
+
+class ProdutoImportado(Produto):
+    def __init__(self,nome,preco,estoque,taxa_importacao):
+        super().__init__(nome,preco,estoque)
+        self.taxa_importacao=taxa_importacao
+    def exibir_detalhes(self):
+        print(f"Produto Importado: {self.nome} | Preço: R${self.preco} | Estoque: {self.estoque} unidades | Taxa de importação: {self.taxa_importacao}")
+
+
+c=ProdutoImportado("Celular",200.0,5,0.15)
+c.exibir_detalhes()
