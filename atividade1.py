cddb7d1b2b3a3a4e3872c1a355ddebf5c378d63e
Mayara Oliveira <mayara.inacio197@gmail.com>
2025-06-14 14:03:02 -0300

    Exercício 4 feito.

--- atividade1.py
+++ atividade1.py
@@ -9,6 +9,9 @@ class Produto:
     
     def preco_final(self):
         print(f"Preço Final: R${self.preco}")
+    
+    def emitir_nota(self):
+        print(f"Nota gerada para {self.nome}.")
 
 p=Produto("Teclado",100.0,20)
 p.exibir_detalhes()
@@ -18,6 +21,8 @@ class ProdutoNacional(Produto):
         super().__init__(nome, preco, estoque)
     def preco_final(self):
         print(f"Preço Final do Produto Nacional: R${self.preco}")
+    def emitir_nota(self):
+        print(f"Nota fiscal nacional para {self.nome}.")
 
 class ProdutoImportado(Produto):
     def __init__(self, nome, preco, estoque, taxa_importacao):
@@ -27,6 +32,8 @@ class ProdutoImportado(Produto):
         print(f"Produto Importado: {self.nome} | Preço: R${self.preco} | Estoque: {self.estoque} unidades | Taxa de importação: {self.taxa_importacao}")
     def preco_final(self):
         print(f"Preço Final do Produto Importado: R${self.preco +(self.preco * self.taxa_importacao)}")
+    def emitir_nota(self):
+        print(f"Nota de importação para {self.nome} com taxa aplicada.")
 
 #pedido do exercício 2
 c = ProdutoImportado("Celular",200.0,5,0.15)
@@ -36,5 +43,9 @@ c.exibir_detalhes()
 c.preco_final()
 n = ProdutoNacional("Cadeira", 150.0, 10)
 n.preco_final()
+
+#teste do exercício 4
+c.emitir_nota()
+n.emitir_nota()
 
 
