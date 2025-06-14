af8d3ca780a4cc2a7b5e73220573af62d9bf016f
Mayara Oliveira <mayara.inacio197@gmail.com>
2025-06-14 13:51:21 -0300

    Exercício 3 pronto =0

--- atividade1.py
+++ atividade1.py
@@ -1,25 +1,40 @@
 class Produto:
-    def __init__(self,nome,preco,estoque):
+    def __init__(self, nome, preco, estoque):
         self.nome=nome
         self.preco=preco
         self.estoque=estoque
+
     def exibir_detalhes(self):
         print(f"Produto:{self.nome} | Preço: R${self.preco} | Estoque:{self.estoque} unidades")
+    
+    def preco_final(self):
+        print(f"Preço Final: R${self.preco}")
 
 p=Produto("Teclado",100.0,20)
 p.exibir_detalhes()
 
 class ProdutoNacional(Produto):
-    def __init__(self,nome,preco,estoque):
-        super().__init__(nome,preco,estoque)
+    def __init__(self, nome, preco, estoque):
+        super().__init__(nome, preco, estoque)
+    def preco_final(self):
+        print(f"Preço Final do Produto Nacional: R${self.preco}")
 
 class ProdutoImportado(Produto):
-    def __init__(self,nome,preco,estoque,taxa_importacao):
-        super().__init__(nome,preco,estoque)
+    def __init__(self, nome, preco, estoque, taxa_importacao):
+        super().__init__(nome, preco, estoque)
         self.taxa_importacao=taxa_importacao
     def exibir_detalhes(self):
         print(f"Produto Importado: {self.nome} | Preço: R${self.preco} | Estoque: {self.estoque} unidades | Taxa de importação: {self.taxa_importacao}")
+    def preco_final(self):
+        print(f"Preço Final do Produto Importado: R${self.preco +(self.preco * self.taxa_importacao)}")
 
-
-c=ProdutoImportado("Celular",200.0,5,0.15)
+#pedido do exercício 2
+c = ProdutoImportado("Celular",200.0,5,0.15)
 c.exibir_detalhes()
+
+#pedido do exercício 3
+c.preco_final()
+n = ProdutoNacional("Cadeira", 150.0, 10)
+n.preco_final()
+
+
