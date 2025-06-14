e09db8775c57578d04c5c8d828776e3b7db9fb14
Mayara Oliveira <mayara.inacio197@gmail.com>
2025-06-14 11:35:03 -0300

    Atividade 1 Completa

--- atividade1.py
+++ atividade1.py
@@ -1,1 +1,10 @@
-a=1
+class Produto:
+    def __init__(self,nome,preco,estoque):
+        self.nome=nome
+        self.preco=preco
+        self.estoque=estoque
+    def exibir_detalhes(self):
+        print(f"Produto:{self.nome} | Preço: R${self.preco} | Estoque:{self.estoque} unidades")
+        pass
+p=Produto("Teclado",100.0,20)
+p.exibir_detalhes()
